4f73aacbf58aca9c544e79f945bd422227673d03
ValcasaraBryan <bryanvalcasara@live.fr>
2019-02-08 12:44:51 +0100

    ajout d'une carre

--- srcs/main.c
+++ srcs/main.c
@@ -13,13 +13,23 @@
 #include "lem-in.h"
 #include <mlx.h>
 
-typedef struct    data_s
+typedef struct		data_s
 {
-    void          *mlx_ptr;
-    void          *mlx_win;
-}                 data_t;
+	void			*mlx_ptr;
+	void			*mlx_win;
+	int				longueur_win;
+	int				largeur_win;
+	int				color;
+	int				color_carre_x;
+	int				color_carre_y;
+	int				color_interieur;
+	int				centre_x;
+	int				centre_y;
+	int				longueur;
+	int				largeur;
+}					data_t;
 
-int			key_hook(int keycode, data_t *param)
+int			key_hook(int keycode, data_t *p)
 {
 	int i;
 	int j;
@@ -30,13 +40,45 @@ int			key_hook(int keycode, data_t *param)
 	else if (keycode == 53)
 		printf("ESC\n");
 	else if (keycode == 126)
-		printf("HAUT\n");
+	{
+		p->color++;
+		printf("HAUT %d\n", p->color);
+	}
 	else if (keycode == 125)
-		printf("BAS\n");
+	{
+		p->color--;
+		printf("BAS %d\n", p->color);
+	}
 	else if (keycode == 123)
-		printf("GAUCHE\n");
+	{
+		p->color -= 15;
+		printf("GAUCHE %d\n", p->color);
+	}
 	else if (keycode == 124)
-		printf("DROIT\n");
+	{
+		p->color += 15;
+		printf("DROIT %d\n", p->color);
+	}
+	else if (keycode == 67)
+	{
+		p->color *= 10;
+		printf("* %d\n", p->color);
+	}
+	else if (keycode == 69)
+	{
+		p->color += 100;
+		printf("+ %d\n", p->color);
+	}
+	else if (keycode == 78)
+	{
+		p->color -= 100;
+		printf("- %d\n", p->color);
+	}
+	else if (keycode == 75)
+	{
+		p->color /= 10;
+		printf("/ %d\n", p->color);
+	}
 	else
 		printf("%d\n", keycode);
 	while (i < 500)
@@ -44,7 +86,24 @@ int			key_hook(int keycode, data_t *param)
 		j = 0;
 		while (j < 500)
 		{
-			mlx_pixel_put(param->mlx_ptr, param->mlx_win, i, j, keycode * 50);	
+			mlx_pixel_put(p->mlx_ptr, p->mlx_win, i, j, p->color);	
+			j++;
+		}
+		i++;
+	}
+	i = p->centre_x;
+	while (i < p->centre_x + p->largeur)
+	{
+		j = p->centre_y;
+		while (j < p->centre_y + p->longueur)
+		{
+			if (i != p->centre_x && i != p->centre_x + p->largeur - 1
+				&& j != p->centre_y && j != p->centre_y + p->longueur - 1)
+				mlx_pixel_put(p->mlx_ptr, p->mlx_win, i, j, p->color_interieur);
+			if (i == p->centre_x || i == p->centre_x + p->largeur - 1)
+				mlx_pixel_put(p->mlx_ptr, p->mlx_win, i, j, p->color_carre_x);			
+			if (j == p->centre_y || j == p->centre_y + p->longueur - 1)
+				mlx_pixel_put(p->mlx_ptr, p->mlx_win, i, j, p->color_carre_y);
 			j++;
 		}
 		i++;
@@ -109,11 +168,20 @@ int			main(int argc, char **argv)
 	ft_put_list(infos.file);
 	ft_fprintf("OK\n", 1);
 	ft_fprintf("\\-------------------------------------------/\n\n\n", 2);
-	erase_infos(&infos);
-	erase_data(&infos);
-	
-	p.mlx_win = mlx_new_window(p.mlx_ptr, 500, 500, "mlx 42");
+	p.color = 0;
+	p.color_carre_x = 100000000;
+	p.color_carre_y = 100000000;
+	p.color_interieur = 54461616;
+	p.longueur_win = 500;
+	p.largeur_win = 500;
+	p.longueur = 50;
+	p.largeur = 50;
+	p.centre_x = infos.data[0].coor_x + (p.longueur / 2); // centre du carre
+	p.centre_y = infos.data[0].coor_y + (p.largeur / 2); // centre du carre
+	p.mlx_win = mlx_new_window(p.mlx_ptr, p.longueur_win, p.largeur_win, "mlx 42");
 	mlx_key_hook(p.mlx_win, key_hook, &p);
 	mlx_loop(p.mlx_ptr);
+	erase_infos(&infos);
+	erase_data(&infos);
 	return (0);
 }
