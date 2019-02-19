cf001cac5c1db40d9fdcd2f50cf58818a301953a
ValcasaraBryan <bryanvalcasara@live.fr>
2019-02-19 20:41:21 +0100

    ajout de bonus ajout de grille pour mettre les pipe sur le graphique

--- srcs/main.c
+++ srcs/main.c
@@ -15,7 +15,10 @@
 
 typedef struct		data_s
 {
+	int				index_of_box;
 	int				nb_of_box;
+	int				*grille_x;
+	int				*grille_y;
 	void			*mlx_ptr;
 	void			*mlx_win;
 	int				longueur_win;
@@ -28,8 +31,77 @@ typedef struct		data_s
 	int				centre_y;
 	int				longueur;
 	int				largeur;
+	int				medium;
+	int				maximum_x;
+	int				maximum_y;
+	int				coef;
+	int				**mappage_box;
 	t_infos			*infos;
 }					data_t;
+
+int			init_grille_x(data_t *p, char c)
+{
+	int		i;
+	int		j;
+	int		divi;
+	int		nb_of_line;
+	int		res;
+
+	i = 0;
+	nb_of_line = p->medium + 1;
+	i = 0;
+	if (c == 'x' || c == 'y')
+	{
+		divi = p->longueur_win / nb_of_line;
+		while (i < nb_of_line)
+		{
+			j = 0;
+			res = divi;
+			while (j < i)
+			{
+				res += divi;
+				j++;
+			}
+			p->grille_x[i] = res;
+			i++;
+		}
+	}
+	else
+		return (0);
+	return (1);
+}
+
+int			init_grille_y(data_t *p, char c)
+{
+	int		i;
+	int		j;
+	int		divi;
+	int		nb_of_line;
+	int		res;
+
+	i = 0;
+	nb_of_line = p->medium + 1;
+	i = 0;
+	if (c == 'x' || c == 'y')
+	{
+		divi = p->largeur_win / nb_of_line;
+		while (i < nb_of_line)
+		{
+			j = 0;
+			res = divi;
+			while (j < i)
+			{
+				res += divi;
+				j++;
+			}
+			p->grille_y[i] = res;
+			i++;
+		}
+	}
+	else
+		return (0);
+	return (1);
+}
 
 int			val_max_coor(t_data *data, char c)
 {
@@ -44,11 +116,121 @@ int			val_max_coor(t_data *data, char c)
 			if (data[i].coor_x > val)
 				val = data[i].coor_x;
 		if (c == 'y')
-		if (data[i].coor_y > val)
-			val = data[i].coor_y;
+			if (data[i].coor_y > val)
+				val = data[i].coor_y;
 		i++;
 	}
 	return (val);
+}
+
+int			init_tab_x(data_t *p, int nb_of_line)
+{
+	int		i;
+
+	i = 0;
+	if (!(p->grille_x = (int *)malloc(sizeof(int) * nb_of_line)))
+		return (0);
+	while (i < nb_of_line)
+		p->grille_x[i++] = 0;
+	return (1);
+}
+
+int			init_tab_y(data_t *p, int nb_of_line)
+{
+	int		i;
+
+	i = 0;
+	if (!(p->grille_y = (int *)malloc(sizeof(int) * nb_of_line)))
+		return (0);
+	while (i < nb_of_line)
+		p->grille_y[i++] = 0;
+	return (1);
+}
+
+int			nb_of_tab(t_data *data)
+{
+	int		i;
+
+	i = 0;
+	if (!data)
+		return (-1);
+	while (data[i].name_box)
+		i++;
+	return (i);
+}
+
+int			check_coordonnee(data_t *p, int x, int y)
+{
+	int		i;
+
+	i = 0;
+	while (i < p->medium)
+	{
+		if (x == p->grille_x[i] || y == p->grille_y[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int			check_coordonnee_data(data_t *p, int x, int y)
+{
+	int		i;
+
+	i = 0;
+	while (p->infos->data[i].name_box)
+	{
+		if (p->infos->data[i].coor_x == x && p->infos->data[i].coor_y == y)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int			chemin(data_t *p, t_data *start, t_data *finish)
+{
+	(void)p;
+	(void)start;
+	printf("\033[34;05mx = %d - y = %d\n\033[00m", finish->coor_x, finish->coor_y);
+	return (0);
+}
+
+int			mappage(data_t *p, int x, int y, int val)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		l;
+
+	i = 1;
+	j = -1;
+	k = 1;
+	l = -1;
+	while (x + i < p->medium && x + i >= 0) // gauche
+	{
+		if (p->mappage_box[x + i][y] != 2)
+			p->mappage_box[x + i][y] = val;
+		i++;
+	}
+	while (x + j >= 0 && x + j < p->medium) // droite
+	{
+		if (p->mappage_box[x + j][y] != 2)
+			p->mappage_box[x + j][y] = val;
+		j--;
+	}
+	while (y + k < p->medium && y + k >= 0) // bas
+	{
+		if (p->mappage_box[x][y + k] != 2)
+			p->mappage_box[x][y + k] = val;
+		k++;
+	}
+	while (y + l >= 0 && y + l < p->medium) // haut
+	{
+		if (p->mappage_box[x][y + l] != 2)
+			p->mappage_box[x][y + l] = val;
+		l--;
+	}
+	return (1);
 }
 
 int			key_hook(int keycode, data_t *p)
@@ -57,6 +239,7 @@ int			key_hook(int keycode, data_t *p)
 	int j;
 
 	i = 0;
+	p->color = 5000000;
 	if (keycode == 36)
 		printf("ENTREE\n");
 	else if (keycode == 53)
@@ -79,7 +262,7 @@ int			key_hook(int keycode, data_t *p)
 	else if (keycode == 124)
 	{
 		p->color += 15;
-		printf("DROIT %d\n", p->color);
+		printf("DROIT b%d\n", p->color);
 	}
 	else if (keycode == 67)
 	{
@@ -106,22 +289,99 @@ int			key_hook(int keycode, data_t *p)
 	p->color_carre_x = 100000000;
 	p->color_carre_y = 100000000;
 	p->color_interieur = 54461616;
-	p->longueur = (p->longueur_win * 5) / 100;
-	p->largeur = (p->largeur_win * 5) / 100;
+	p->longueur = p->longueur_win * 0.03;
+	p->largeur = p->largeur_win * 0.03;
 	while (i < p->longueur_win)
 	{
 		j = 0;
 		while (j < p->largeur_win)
 		{
 			mlx_pixel_put(p->mlx_ptr, p->mlx_win, i, j, p->color);	
+			// if (check_coordonnee(p, i, j))
+				// mlx_pixel_put(p->mlx_ptr, p->mlx_win, i, j, 26459932); // affiche les grilles
 			j++;
 		}
 		i++;
 	}
-	while (p->infos->data[p->nb_of_box].name_box)
+	int nb;
+	i = -1;
+	while (i++ < p->medium)
 	{
-		p->centre_y = p->infos->data[p->nb_of_box].coor_y * ((p->longueur_win * 3.5) / 100); // centre du carre
-		p->centre_x = p->infos->data[p->nb_of_box].coor_x * ((p->largeur_win * 3.5) / 100); // centre du carre
+		j = -1;
+		while (j++ < p->medium)
+		{
+			if (check_coordonnee_data(p, j, i))
+				printf("\033[34;01m%3d-%-2d\033[00m", j, i);
+			else
+				printf("%3d-%-2d", j, i);
+		}
+		printf("\n");
+	}
+	i = 0;
+	if (!(p->mappage_box = (int **)malloc(sizeof(int *) * p->medium)))
+		return (0);
+	while (i < p->medium)
+	{
+		if (!(p->mappage_box[i] = (int *)malloc(sizeof(int) * p->medium)))
+			return (0);
+		j = 0;
+		while (j < p->medium)
+			p->mappage_box[i][j++] = 0;
+		i++;
+	}
+	printf("\n");
+	nb = 0;
+	while (nb < p->infos->nb_of_box)
+	{
+		p->mappage_box[p->infos->data[nb].coor_x - 1][p->infos->data[nb].coor_y - 1] = 2;
+		nb++;
+	}
+	nb = 0;
+	while (nb < p->infos->nb_of_box)
+	{
+		i = 0;
+		while (i <= p->medium)
+		{
+			j = 0;
+			while (j <= p->medium)
+			{
+				if (p->infos->data[nb].coor_x == j && p->infos->data[nb].coor_y == i)
+				{
+					mappage(p, j - 1, i - 1, 1);
+				}
+				j++;
+			}
+			i++;
+		}
+		nb++;
+	}
+	i = -1;
+	while (++i < p->medium)
+	{
+		j = -1;
+		while (++j < p->medium)
+		{
+			printf("%2d", p->mappage_box[j][i]);
+		}
+		printf("\n");
+	}
+	i = 0;
+	while (p->infos->data[i].name_box)
+	{
+		j = 0;
+		printf("\033[34;01mx = %d - y = %d\n\033[00m", p->infos->data[i].coor_x,  p->infos->data[i].coor_y);
+		while (j < p->infos->data[i].nb_of_link)
+		{
+			chemin(p, &p->infos->data[i], p->infos->data[i].pipe[j]);
+			j++;
+		}
+		i++;
+	}
+	
+	while (p->infos->data[p->index_of_box].name_box)
+	{
+		p->centre_y = p->grille_y[p->infos->data[p->index_of_box].coor_y - 1] - (p->largeur / 2); // centre du carre + sur une des grille de la fenetre
+		p->centre_x = p->grille_x[p->infos->data[p->index_of_box].coor_x - 1] - (p->longueur / 2); // centre du carre + sur une des grille de la fenetre
 		i = p->centre_x;
 		while (i < p->centre_x + p->longueur)
 		{
@@ -139,10 +399,14 @@ int			key_hook(int keycode, data_t *p)
 			}
 			i++;
 		}
-		printf("x = %d - y = %d\n", p->infos->data[p->nb_of_box].coor_x, p->infos->data[p->nb_of_box].coor_y);
-		p->nb_of_box++;
+		printf("x = %d - y = %d ----\n", p->infos->data[p->index_of_box].coor_x, p->infos->data[p->index_of_box].coor_y);
+		nb = -1;
+		while (++nb < p->infos->data[p->index_of_box].nb_of_link)
+			printf("x = %d, y = %d\n", p->infos->data[p->index_of_box].pipe[nb]->coor_x, p->infos->data[p->index_of_box].pipe[nb]->coor_y);
+		printf("\n");
+		p->index_of_box++;
 	}
-	p->nb_of_box = 0;
+	p->index_of_box = 0;
 	return (0);
 }
 
@@ -205,11 +469,20 @@ int			main(int argc, char **argv)
 	ft_fprintf("\\-------------------------------------------/\n\n\n", 2);
 	p.infos = &infos;
 	p.color = 0;
-	p.nb_of_box = 0;
-	p.longueur_win =  val_max_coor(infos.data, 'x') * 50;
-	p.largeur_win = val_max_coor(infos.data, 'y') * 50;
-	// p.longueur_win = (p.largeur_win < (p.longueur_win * 3)) ? (p.longueur_win / 2) : p.longueur_win;
-	// \p.largeur_win = (p.longueur_win < (p.largeur_win * 2)) ? (p.largeur_win / 2) : p.largeur_win;
+	p.index_of_box = 0;
+	p.nb_of_box = nb_of_tab(infos.data);
+	p.maximum_x = val_max_coor(infos.data, 'x');
+	p.maximum_y = val_max_coor(infos.data, 'y');
+	p.medium = (p.maximum_x > p.maximum_y) ? p.maximum_x : p.maximum_y;
+	p.coef = p.medium * 1;
+	printf("%d medium\n", p.medium);
+	printf("%d, %d- maximum\n", p.maximum_x, p.maximum_y);
+	p.longueur_win = p.medium * p.coef;
+	p.largeur_win = p.medium * p.coef;
+	init_tab_x(&p, p.medium + 1);
+	init_tab_y(&p, p.medium + 1);
+	init_grille_x(&p, 'x');
+	init_grille_y(&p, 'x');
 	printf("%d - %d\n", p.longueur_win, p.largeur_win);
 	p.mlx_win = mlx_new_window(p.mlx_ptr, p.longueur_win, p.largeur_win, "mlx 42");
 	mlx_key_hook(p.mlx_win, key_hook, &p);
